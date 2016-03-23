8369c862e52295f5251c9e973411ebc51110d93f
Hieu Rocker <rockerhieu@gmail.com>
2016-03-23 19:41:28 +0700

    Auto layout for LoginViewController

---
+++ Lab3/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Lab3
+//
+//  Created by Hieu Rocker on 3/23/16.
+//  Copyright © 2016 com.appable. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailView: UITextField!
+    @IBOutlet weak var passwordView: UITextField!
+    
+    
+    
+    @IBAction func onLoginTap(sender: UIButton) {
+        
+    }
+
+    @IBAction func onSignUpTap(sender: UIButton) {
+    }
+}
